5389c778c54c9d36566b7e48ee971e83bb67edcf
zxf2007pk <511376651@qq.com>
2025-11-28 04:43:55 +0800

    Update router.js
    1

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -2,38 +2,18 @@ import douban from './spider/video/douban.js';
 import live from './spider/video/live.js';
 import wogg from './spider/video/wogg.js';
 import mogg from './spider/video/mogg.js';
-import lbpp from './spider/video/lbpp.js';
-import feimao from './spider/video/feimao.js';
-import tudou from './spider/video/tudou.js';
-import xiaoya from './spider/video/xiaoya.js';
 import duoduo from './spider/video/duoduo.js';
 import xiaomi from './spider/video/xiaomi.js';
 import ouge from './spider/video/ouge.js';
-import liuqu from './spider/video/liuqu.js';
 import zhizhen from './spider/video/zhizhen.js';
-import yunpanres from './spider/video/yunpanres.js';
-import xzys from './spider/video/xzys.js';
-import meijumi from './spider/video/meijumi.js';
-import czzy from './spider/video/czzy.js';
-import subaibai from './spider/video/subaibai.js';
-import hezi from './spider/video/hezi.js';
 import ikanbot from './spider/video/ikanbot.js';
-import nangua from './spider/video/ng.js';
-import ttian from './spider/video/ttian.js';
-import zxzj from './spider/video/zxzj.js';
-import ddys from './spider/video/ddys.js';
-import nongmin from './spider/video/nongmin.js';
-import rrys from './spider/video/rrys.js';
-import klm from './spider/video/klm.js';
 import cntv from './spider/video/cntv.js';
 import huya from './spider/video/huya.js';
 import douyu from './spider/video/douyu.js';
 import bili from './spider/video/bili.js';
-import clicli from './spider/video/clicli.js';
 import _360ba from './spider/video/_360ba.js';
 import m3u8cj from './spider/video/m3u8cj.js';
 import appys from './spider/video/appys.js';
-import maiyoux from './spider/video/maiyoux.js';
 import push from './spider/video/push.js';
 import baseset from './spider/video/baseset.js';
 import alist from './spider/pan/alist.js';
@@ -49,7 +29,7 @@ import coco from './spider/book/coco.js';
 
 
 
-const spiders = [douban,live,wogg,mogg,lbpp,feimao,tudou,duoduo,xiaomi,ouge,liuqu,zhizhen,xiaoya,yunpanres,xzys,meijumi,czzy,subaibai,hezi,ikanbot,nangua,ttian,zxzj,ddys,nongmin,rrys,klm,cntv,huya,douyu,bili,clicli,_360ba,m3u8cj,appys,maiyoux, push, baseset, alist, _13bqg, laobaigs,ts230,bookan, copymanga,bg,fengche,baozimh,coco];
+const spiders = [douban,live,wogg,mogg,duoduo,ouge,zhizhen,ikanbot,cntv,huya,douyu,bili,_360ba,m3u8cj,appys,push, baseset, alist, _13bqg, laobaigs,ts230,bookan, copymanga,bg,fengche,baozimh,coco];
 const spiderPrefix = '/spider';
 
 
